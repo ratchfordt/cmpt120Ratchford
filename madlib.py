b5cde39992e9fa87b37707c420b39e8f3179bd66
Trevor Ratchford <twnman02@gmail.com>
2018-09-11 17:47:12 -0400

    MadLibs project for lab 2

---
+++ madlib.py
@@ -0,0 +1,10 @@
+# Mad Libs
+
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+    print("Go to ", place, " and ", verb, " my ", adjective, noun)
+
+main()
