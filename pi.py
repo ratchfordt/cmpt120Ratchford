63c1eb3182afc1b9a0f9fa6444c3114ce8d2d621
Trevor Ratchford <twnman02@gmail.com>
2018-09-18 18:15:14 -0400

    Modified variable names to be less confusing.

--- pi.py
+++ pi.py
@@ -4,14 +4,14 @@ import math
 
 
 def main():
-    x = 1
+    isEven = 1
     appx = 0
     print("Approximate Pi")
     n = eval(input("How many degrees would you like to approximate pi to? "))
     for i in range(n):
-        x += 1
+        isEven += 1
         d = (1 + i * 2)
-        if (x % 2) != 0:
+        if (isEven % 2) != 0:
             appx -= 4/d
         else:
             appx += 4/d
